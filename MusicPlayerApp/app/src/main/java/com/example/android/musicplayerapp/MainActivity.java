1c48384136efdc30b752a13d2e8db76e29cc2db2
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2017-02-08 18:02:03 +0530

    toast added to show at the end of song

--- MusicPlayerApp/app/src/main/java/com/example/android/musicplayerapp/MainActivity.java
+++ MusicPlayerApp/app/src/main/java/com/example/android/musicplayerapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "I am done", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
